6d7f93bb2b05c6953a0a5380df7c18f228d77b9c
Gongi-ADL <122498504+Gongi-ADL@users.noreply.github.com>
2023-01-14 12:47:47 -0400

    updating new files

---
+++ server/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyparser from 'body-parser';
+import dotenv from 'dotenv'
+import bd from './db/connection.js'
+dotenv.config()
+
+const app = express()
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded( {extended: true} ));
+
+const dbConnection = async() => {
+    try {
+
+        await bd.authenticate();
+        console.log('You are succesfully connected to the database')
+
+    } catch (error) {
+
+        throw new Error(error)
+
+    }
+}
+
+app.use(dbConnection(), ()=> {
+    dbConnection()
+})
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`The app is running on ${process.env.SERVER_PORT}`)
+})
